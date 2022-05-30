d72ff81b385f93239556bd23c63fe0de52f50702
Mahmoud Mardawi <Mahmoud.Mardawi@foothillsolutions.com>
2022-05-30 10:50:37 +0300

    Create DoctorWhoCoreDbContext and add it to DoctorWho.Db

---
+++ DoctorWho.Db/Contexts/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DoctorWho.Db.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db.Contexts
+{
+    internal class DoctorWhoCoreDbContext : DbContext
+    {
+        public static DoctorWhoCoreDbContext _context = new DoctorWhoCoreDbContext();
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Companion> Companions { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<EpisodeCompanion> EpisodeCompanions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog=DoctorWhoCore")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+                .EnableSensitiveDataLogging(true);
+        }
+
+    }
+}
