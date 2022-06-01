542e161392e43fe55f2660aa5bd6fb18fc6249a5
MahmoudMardawi <ma.mardawi@gmail.com>
2022-06-01 20:16:59 +0300

    Execute builders and test solution

--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using DoctorWho.Db.Contexts;
+using System.Data.Entity;
 
-namespace DoctorWho
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<DoctorWhoCoreDbContext>(options =>
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DoctorWhoCore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+});
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<DoctorWhoCoreDbContext>())
+{
+    context.Database.EnsureCreated();
 }
