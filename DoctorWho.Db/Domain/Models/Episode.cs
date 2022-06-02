bab9eec0fb8c596baf976dab2a98cfc1de9517a0
MahmoudMardawi <ma.mardawi@gmail.com>
2022-06-02 18:46:57 +0300

    Add nullable option in Notes column

--- DoctorWho.Db/Domain/Models/Episode.cs
+++ DoctorWho.Db/Domain/Models/Episode.cs
@@ -19,10 +19,10 @@ namespace DoctorWho.Db.Domain.Models
         public int EpisodeNumber { get; set; }
         public string EpisodeType { get; set; }
         public string Title { get; set; }
+        public string? Notes { get; set; }
         public DateTime EpisodeDate { get; set; }
         public Author Author { get; set; }
         public int AuthorId { get; set; }
-        public string Text { get; set; }
         public Doctor Doctor { get; set; }
         public int DoctorId { get; set; }
         public List<EpisodeCompanion> EpisodeCompanions { get; set; }
