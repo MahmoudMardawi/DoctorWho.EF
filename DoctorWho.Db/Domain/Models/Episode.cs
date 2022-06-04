e75f3ad513e47cadc84a87ea375e9b182b911507
MahmoudMardawi <ma.mardawi@gmail.com>
2022-06-04 15:15:58 +0300

    Create constructor to provide Create/Update/Delete methods

--- DoctorWho.Db/Domain/Models/Episode.cs
+++ DoctorWho.Db/Domain/Models/Episode.cs
@@ -14,6 +14,21 @@ namespace DoctorWho.Db.Domain.Models
             this.EpisodeCompanions = new List<EpisodeCompanion>();
             this.EpisodeEnemies = new List<EpisodeEnemy>();
         }
+
+        public Episode(int DoctorId, int? SeriesNumber, int? EpisodeNumber, string EpisodeType,
+            string Title, DateTime? EpisodeDate, int AuthorId, string Notes) : this()
+        {
+            this.DoctorId = DoctorId;
+            this.SeriesNumber = (int)SeriesNumber;
+            this.EpisodeNumber = (int)EpisodeNumber;
+            this.EpisodeType = EpisodeType;
+            this.Title = Title;
+            this.EpisodeDate = (DateTime)EpisodeDate;
+            this.AuthorId = AuthorId;
+            this.DoctorId = DoctorId;
+            this.Notes = Notes;
+        }
+
         public int EpisodeId { get; set; }
         public int SeriesNumber { get; set; }
         public int EpisodeNumber { get; set; }
@@ -27,5 +42,8 @@ namespace DoctorWho.Db.Domain.Models
         public int DoctorId { get; set; }
         public List<EpisodeCompanion> EpisodeCompanions { get; set; }
         public List<EpisodeEnemy> EpisodeEnemies { get; set; }
+        public int? SeriesNumber1 { get; }
+        public int? EpisodeNumber1 { get; }
+        public DateTime? EpisodeDate1 { get; }
     }
 }
