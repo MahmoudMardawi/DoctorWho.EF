6b4c9879f644fff360f8960f6784cf6f23e55e68
MahmoudMardawi <ma.mardawi@gmail.com>
2022-06-04 15:19:48 +0300

    Create AddCompanionToEpisode method and put in right place

---
+++ DoctorWho.Db/Repositoris/EpisodeCompanionsRepository.cs
@@ -0,0 +1,25 @@
+﻿using DoctorWho.Db.Contexts;
+using DoctorWho.Db.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db.Repositoris
+{
+    public class EpisodeCompanionsRepository
+    {
+        public static void AddCompanionToEpisode(Companion Companion, int EpisodeId)
+        {
+            if (Companion == null) throw new ArgumentNullException("Invalid input! Please provide a companion infomation");
+            var episode = DoctorWhoCoreDbContext._context.Episodes.Find(EpisodeId);
+            if (episode != null)
+            {
+                episode.EpisodeCompanions.Add(new EpisodeCompanion { CompanionId = Companion.CompanionId, EpisodeId = EpisodeId });
+                DoctorWhoCoreDbContext._context.SaveChanges();
+            }
+            else throw new InvalidOperationException("Invalid provided Id!");
+        }
+    }
+}
