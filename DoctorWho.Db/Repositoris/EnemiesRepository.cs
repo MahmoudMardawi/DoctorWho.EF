242298445cfe58a3cf68d11e4b70e5187989c7cb
MahmoudMardawi <ma.mardawi@gmail.com>
2022-06-04 15:17:20 +0300

    Create methods to Create/Update/Delete and GetEnemyById

---
+++ DoctorWho.Db/Repositoris/EnemiesRepository.cs
@@ -0,0 +1,43 @@
+﻿using DoctorWho.Db.Contexts;
+using DoctorWho.Db.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db.Repositoris
+{
+    public class EnemiesRepository
+    {
+        public static void Create(string EnemyName, string Description)
+        {
+            if (EnemyName == null) throw new ArgumentNullException("EnemyName must not be null!");
+            DoctorWhoCoreDbContext._context.Enemies.Add(new Enemy(EnemyName, Description));
+            DoctorWhoCoreDbContext._context.SaveChanges();
+        }
+        public static void Update()
+        {
+            DoctorWhoCoreDbContext._context.ChangeTracker.DetectChanges();
+            DoctorWhoCoreDbContext._context.SaveChanges();
+        }
+        public static void Delete(Enemy Enemy)
+        {
+            if (Enemy == null) throw new ArgumentNullException("There is not Enemy in the Enemies table");
+            try
+            {
+                DoctorWhoCoreDbContext._context.Enemies.Remove(Enemy);
+                DoctorWhoCoreDbContext._context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static Enemy GetEnemyById(int EnemyId)
+        {
+            var enemy = DoctorWhoCoreDbContext._context.Enemies.Find(EnemyId);
+            return enemy != null ? enemy : throw new NullReferenceException("No enemies with the provided Id exist in the database!");
+        }
+    }
+}
